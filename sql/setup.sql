5ad748afb5405a03d96158a2171561c44611eb32
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-17 10:43:44 -0700

    wrote setup.sql for mangaka

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,9 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS mangaka;
+
+CREATE TABLE mangaka (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    series TEXT NOT NULL,
+)
