89e4860bc4b693da29a127d6e3b63a9c1d791a9f
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-17 11:30:24 -0700

    Wrote test for get all mangaka

--- __tests__/mangaka.test.js
+++ __tests__/mangaka.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { createMangaka } = require('../lib/models/Mangaka');
 
 describe('backend-hand-of-resources routes', () => {
   beforeEach(() => {
@@ -16,6 +17,17 @@ describe('backend-hand-of-resources routes', () => {
     const res = await request(app)
       .post('/api/v1/mangaka')
       .send({ name: 'Eiichiro Oda', series: 'One Piece' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'Eiichiro Oda',
+      series: 'One Piece',
+    });
+  });
+  it('should be able to list all mangaka', async () => {
+    await createMangaka({ name: 'Eichiiro Oda', series: 'One Piece' });
+
+    const res = await request(app).get('/api/v1/mangaka');
 
     expect(res.body).toEqual({
       id: expect.any(String),
