67f20524deb727e7fa095f958fa354bd96555fa0
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-18 16:58:45 -0700

    wrote test for list all bikes

--- __tests__/bikes.test.js
+++ __tests__/bikes.test.js
@@ -2,6 +2,7 @@ const app = require('../lib/app');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const Bike = require('../lib/models/Bikes');
 
 describe('backend-hand-of-resources routes', () => {
   beforeEach(() => {
@@ -12,7 +13,7 @@ describe('backend-hand-of-resources routes', () => {
     pool.end();
   });
 
-  it.only('should be able to create a bike', async () => {
+  it('should be able to create a bike', async () => {
     const res = await request(app)
       .post('/api/v1/bike')
       .send({ brand: 'Honda', type: 'Grom' });
@@ -22,5 +23,14 @@ describe('backend-hand-of-resources routes', () => {
       brand: 'Honda',
       type: 'Grom',
     });
+  });
+
+  it('Should be able to list all bike', async () => {
+    const bike1 = await Bike.createBike({ brand: 'Honda', type: 'Grom' });
+    const bike2 = { brand: 'Harley', type: 'Iron 833' };
+
+    const res = await request(app).get('/api/v1/bike');
+
+    expect(res.body).toEqual([bike1, bike2]);
   });
 });
