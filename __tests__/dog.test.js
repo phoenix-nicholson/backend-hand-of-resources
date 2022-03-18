b8665c3c4147a656f88de0086ea63a2e5290742b
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-18 09:22:06 -0700

    renaming miklo file tree to dog to make it make more sense, sorry.. not sorry?

--- __tests__/dog.test.js
+++ __tests__/dog.test.js
@@ -2,6 +2,8 @@ const setup = require('../data/setup');
 const app = require('../lib/app');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
+const { createMiklo } = require('../lib/models/Dog');
+const Miklo = require('../lib/models/Dog');
 
 describe('backend-hand-of-resources routes', () => {
   beforeEach(() => {
@@ -14,7 +16,7 @@ describe('backend-hand-of-resources routes', () => {
 
   it('should be able to create a dog', async () => {
     const res = await request(app)
-      .post('/api/v1/miklo')
+      .post('/api/v1/dog')
       .send({ name: 'Miklo', favtoy: 'Soccer Ball' });
 
     expect(res.body).toEqual({
@@ -22,5 +24,13 @@ describe('backend-hand-of-resources routes', () => {
       name: 'Miklo',
       favtoy: 'Soccer Ball',
     });
+  });
+
+  it('Should be able to list all dogs', async () => {
+    // const miklo1 = await Miklo.createMiklo({
+    //   name: 'Miklo',
+    //   favtoy: 'Soccer Ball'
+    // });
+    // const miklo2 =
   });
 });
